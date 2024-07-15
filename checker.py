f5c52a2235c73b781d5c17dda1136cd62eb77b9e
Fernando <14892931+FMR7@users.noreply.github.com>
2024-07-15 10:26:50 +0200

    Script update

--- checker.py
+++ checker.py
@@ -15,10 +15,12 @@ def checkCards():
           data = (f"{card:<{col_size}} | {name:<{col_size}} | {powerName:<{col_size}}")
           location_inconsistencies.append(data)
     
-    print(f'INCONSISTENCIES FOUND: {inconsistencies_counter}')
+    print(f'INCONSISTENCIES FOUND IN NAMING: {inconsistencies_counter}')
     print()
-    for inconsistency in location_inconsistencies:
-      print(inconsistency)
+    if(inconsistencies_counter > 0):
+        print(f"{'ID':<{col_size}}   {'Card Name':<{col_size}}   {'Power Name':<{col_size}}")
+        for inconsistency in location_inconsistencies:
+            print(inconsistency)
     
 def getPowerName(powerId):
   with open('powers.json', 'r') as f:
